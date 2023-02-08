296337b4595fb7af29bffaa87dca6eb5c60bf65d
JoshRiggs01 <5574@holbertonstudents.com>
2023-02-08 20:27:46 +0000

    baseodel update

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-
-import uuid
-from datetime import datetime
+import datetime
 
 class BaseModel:
-    """This class defines the common attributes and methods for other classes"""
-    
-    def __init__(self):
-        """Initializes a BaseModel instance with a unique id, created_at and updated_at attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if 'created_at' in kwargs:
+                self.created_at = datetime.datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        """Returns a string representation of the instance in the format:
-        [<class name>] (<self.id>) <self.__dict__>"""
-        return '[{}] ({}) {}'.format(
-            self.__class__.__name__, self.id, self.__dict__)
-    
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
     def save(self):
-        """Updates the updated_at attribute with the current datetime"""
-        self.updated_at = datetime.now()
-    
+        self.updated_at = datetime.datetime.now()
+
     def to_dict(self):
-        """Returns a dictionary representation of the instance, with added class name and converted created_at and updated_at to ISO format strings"""
-        dictionary = self.__dict__.copy()
-        dictionary['__class__'] = self.__class__.__name__
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        dict_representation = self.__dict__.copy()
+        dict_representation['__class__'] = self.__class__.__name__
+        dict_representation['created_at'] = self.created_at.isoformat()
+        dict_representation['updated_at'] = self.updated_at.isoformat()
+        return dict_representation
+
