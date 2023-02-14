8fc728aa5b9aab4a6e4ecb8445164713a2e28f98
JoshRiggs01 <5574@holbertonstudents.com>
2023-02-14 16:41:29 +0000

    fixing base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
-import datetime
+
+import uuid
+from datetime import datetime
 
 class BaseModel:
+    """Defines all common attributes/methods for other classes"""
+
     def __init__(self, *args, **kwargs):
+        """Initializes a new instance"""
         if kwargs:
             for key, value in kwargs.items():
-                if key != '__class__':
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != "__class__":
                     setattr(self, key, value)
-            if 'created_at' in kwargs:
-                self.created_at = datetime.datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            if 'updated_at' in kwargs:
-                self.updated_at = datetime.datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        """Updates the updated_at attribute with the current datetime"""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        dict_representation = self.__dict__.copy()
-        dict_representation['__class__'] = self.__class__.__name__
-        dict_representation['created_at'] = self.created_at.isoformat()
-        dict_representation['updated_at'] = self.updated_at.isoformat()
-        return dict_representation
+        """Returns a dictionary containing all keys/values of __dict__"""
+        result = self.__dict__.copy()
+        result['__class__'] = type(self).__name__
+        result['created_at'] = result['created_at'].isoformat()
+        result['updated_at'] = result['updated_at'].isoformat()
+        return result
 
